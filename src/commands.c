412c692e9f43268df51ca52fc99b9797f35b8a95
parkjuwon <pjw930731@naver.com>
2018-04-01 18:00:38 +0900

    Update code
    * PWD, CD done. Add TODO alias

--- src/commands.c
+++ src/commands.c
@@ -1,5 +1,5 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
@@ -7,6 +7,9 @@ int do_cd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
+  if (chdir(argv[1]) < 0)
+    return -1;
+
   return 0;
 }
 
@@ -15,6 +18,9 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
+  char Buf[512];
+  getcwd(Buf, 512);
+  printf("%s\n", Buf);
 
   return 0;
 }
@@ -22,11 +28,15 @@ int do_pwd(int argc, char** argv) {
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc == 2 && strcmp(argv[0], "cd") == 0)
+    return 1;
+  else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc == 1 && strcmp(argv[0], "pwd") == 0)
+    return 1;
+  else return 0;
 }
 
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,30 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  *argv = (char**)malloc(sizeof(char*)*16);
+  *argc = 0;
+  char* buf = strdup(command);
+
+  char* temp = strtok(buf, " \n\t");
+ 
+  while(temp != NULL)
+  {
+     (*argv)[*argc] = (char*)malloc(sizeof(char)*16);
+     (*argv)[*argc] = strdup(temp);
+     (*argc)++;
+     temp = strtok(NULL, " \n\t");
+  }
+
+  if((*argc) == 0)
+  {
+     (*argv)[*argc] = strdup("");
+     (*argc)++;
+
+  }
 }
